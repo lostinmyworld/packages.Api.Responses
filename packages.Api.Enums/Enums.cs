529e1438db69cc03acaeae35283eadc956a87289
Panagiotis Katsampiris <lostinmyworld17@hotmail.com>
2021-03-03 17:45:14 +0200

    projects added.nuget added.

---
+++ packages.Api.Enums/Enums.cs
@@ -0,0 +1,21 @@
+﻿namespace packages.Api.Enums
+{
+    public enum ResponseCode
+    {
+        Ambiguous,
+        Valid,
+        Invalid
+    }
+
+    public enum ErrorCodeEnum
+    {
+        Unexpected,
+        RequestParameterNull,
+        InvalidAuthenticationCredentials,
+        UnauthorizedUser,
+        MissingConfiguration,
+        EmptyResponse,
+        InsertAlreadyExists,
+        EntityNotFound
+    }
+}
